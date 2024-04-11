54498e02f9a4fbb48988f1719b1fe3c9941523b3
Julian Salazar <juliansalazarvelez94@gmail.com>
2024-04-10 19:04:18 -0500

    Cuarta modificacion

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> Tercera modificacion
+          Edit <code>src/App.jsx</code> Cuarta modificacion
         </p>
       </div>
       <p className="read-the-docs">
